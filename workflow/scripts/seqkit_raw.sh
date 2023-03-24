66d087a9a4f5842f0d905114347bc75511302163
tmbuza <ndelly@gmail.com>
2023-03-24 12:01:33 -0500

    Update seqkit_raw.sh

--- workflow/scripts/seqkit_raw.sh
+++ workflow/scripts/seqkit_raw.sh
@@ -7,3 +7,12 @@ SEQKIT="results/qc/seqkit1"
 mkdir -p "${SEQKIT}"
 seqkit stat "${INPUTDIR}"/*.fastq.gz >"${SEQKIT}"/seqkit_stats.txt
 
+
+# # INPUTDIR="resources/reads"
+# INPUTDIR="resources/test"
+# # SEQKIT="results/qc/seqkit1"
+# SEQKIT="results/seqkit1"
+# mkdir -p "${SEQKIT}"
+# # seqkit stat "${INPUTDIR}"/*.fastq.gz >"${SEQKIT}"/seqkit_stats.txt
+# seqkit stat "${INPUTDIR}"/*.fastq.gz >"${SEQKIT}"/seqkit_stats.txt
+
